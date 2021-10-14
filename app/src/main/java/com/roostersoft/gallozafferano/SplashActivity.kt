eeea7a4d8a6bcb61a95a1b865059b89e8d637c87
Alex <akip-core@outlook.com>
2021-10-14 20:43:58 +0200

    delay on splash, just for watching it

--- app/src/main/java/com/roostersoft/gallozafferano/SplashActivity.kt
+++ app/src/main/java/com/roostersoft/gallozafferano/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.roostersoft.gallozafferano
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_splash)
+        val intent = Intent(this,LoginActivity::class.java)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            // Delay per vedere meglio lo splash
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
