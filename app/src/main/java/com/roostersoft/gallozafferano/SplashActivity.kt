44e0bb08c53c53af36ddb269e77dea55376fe3b0
Alex <akip-core@outlook.com>
2021-10-14 22:51:52 +0200

    shared preferences and others tune up

--- app/src/main/java/com/roostersoft/gallozafferano/SplashActivity.kt
+++ app/src/main/java/com/roostersoft/gallozafferano/SplashActivity.kt
@@ -1,16 +1,26 @@
 package com.roostersoft.gallozafferano
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val intent: Intent
+        val handler = Handler(Looper.getMainLooper())
         //setContentView(R.layout.activity_splash)
-        val intent = Intent(this,LoginActivity::class.java)
-        val handler = Handler()
+        //valore da salvare - shared preferences
+        val userSharedPref = getSharedPreferences(
+            "user", Context.MODE_PRIVATE)
+        if (userSharedPref.getString("user", null)==null){
+            intent = Intent(this,LoginActivity::class.java)
+        }else{
+            intent = Intent(this, MainActivity::class.java)
+        }
         handler.postDelayed(Runnable {
             // Delay per vedere meglio lo splash
             startActivity(intent)
