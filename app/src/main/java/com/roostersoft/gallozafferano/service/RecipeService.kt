1ca1fd8a3ab00e2b1b15a682e77577e56da0786c
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-15 12:04:17 +0200

    Update RecipeService

--- app/src/main/java/com/roostersoft/gallozafferano/service/RecipeService.kt
+++ app/src/main/java/com/roostersoft/gallozafferano/service/RecipeService.kt
@@ -1,11 +1,19 @@
 package com.roostersoft.gallozafferano.service
 
+import com.roostersoft.gallozafferano.model.Recipe
 import com.roostersoft.gallozafferano.model.RecipeWithId
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface RecipeService {
 
     @GET("recipes")
     fun getRecipes(): Call<List<RecipeWithId>>
+
+    @POST("recipes")
+    @FormUrlEncoded
+    fun postRecipe(@Body recipe: Recipe): Call<Recipe>
+
+    @DELETE("recipes/{id}")
+    fun deleteRecipe(@Path("id") id: String): Call<Recipe>
 }
